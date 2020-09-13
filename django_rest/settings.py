b8b256441ea99c65721e015d14ae1a38e5c78c73
虫师 <353332934@qq.com>
2020-09-13 15:59:22 +0800

    Update settings.py
    update setting.py

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -123,5 +123,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
